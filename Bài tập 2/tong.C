edf222868b443d929953a0b65b6e72686f556e2e
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2020-12-03 10:07:42 +0700

    Create tong.C

---
+++ Bài tập 2/tong.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+  //khai bao bien a,b
+  int a,b;
+  //hien thi dong chu "nhap a =" 
+  printf("nhap a=");
+  scanf("%d",&a);
+  //hien thi dong chu "nhap b =" 
+  printf("nhap b=");
+  scanf("%d",&b);
+  //hien thi phep tinh a + b
+  printf ("a + b = %d",a + b);
+  return 0;
+}
