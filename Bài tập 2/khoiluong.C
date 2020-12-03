d4aefc81a8f7dd9019b8619e4af27c4a1a961bef
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2020-12-03 10:54:57 +0700

    Update khoiluong.C

--- Bài tập 2/khoiluong.C
+++ Bài tập 2/khoiluong.C
@@ -4,6 +4,7 @@ int main(void){
   scanf("%d", &m);
   printf("m = %dg\n", m * 1000);
   printf("KG  | G\n");
+  printf("----------\n");
   printf("%d", m);
   printf("   |%d", m * 1000);
   return 0;
