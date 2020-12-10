b58b7bb8eedbae48de1dae04d0872cc4138d89ba
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2020-12-10 09:04:49 +0700

    Update amduong.c

--- Bài tập 3/amduong.c
+++ Bài tập 3/amduong.c
@@ -9,10 +9,12 @@ int main() {
   // kiem tra a la duong hay am
   if ( a > 0 ){
     printf ("%d la so duong ",a);
+    //hien thi a la so duong
   }else if ( a == 0){
     printf (" a = 0");
   }else {
     printf ("%d la so am ",a);
+    //hien thi a la so am
   }
 
   getchar ();
