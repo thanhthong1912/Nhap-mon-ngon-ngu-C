21781a41a5b2d747f852be8503f0a2a3dcbfdf02
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2020-12-17 08:50:57 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
-int main(){
-  int dongia, soluong, thanhtien;
-  printf("don gia:", dongia);
-  scanf("%d", &dongia);
-  printf("so luong:\n", soluong);
-  thanhtien = dongia * soluong;
-  if(soluong>=5);
-  printf("thanhtien = %d\n", thanhtien * 75/100);
-  getchar();
+int main(void) {
+			// khai bao bien dongia, so luong va thanh tien
+int dongia, soluong, thanhtien;
+			// nhap so tu ban phim
+  		printf("nhap don gia :"); 
+	scanf("%d", &dongia);
+		printf("nhap so luong : ");
+	scanf("%d", &soluong);
+			// cong thuc tinh thanh tien
+thanhtien = dongia * soluong;
+			// so luong mua > hoac = 5 thi giam 25%
+	if (soluong >= 5 , dongia >= 500){
+  		printf("so tien can phai thanh toan la %d", thanhtien *50/100);
+}else{
+  		printf("so tien can phai thanh toan la %d", thanhtien);
+			// neu nho hon 5 thi khong giam gia
+}
   return 0;
 }
