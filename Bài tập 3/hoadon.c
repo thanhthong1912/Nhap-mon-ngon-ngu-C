4b463438ec363b3e08ba6f566b7547bd442a8095
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2020-12-17 08:40:06 +0700

    Create hoadon.c

---
+++ Bài tập 3/hoadon.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  int dongia, soluong, thanhtien;
+  printf("don gia:", dongia);
+  scanf("%d", &dongia);
+  printf("so luong:\n", soluong);
+  thanhtien = dongia * soluong;
+  if(soluong>=5);
+  printf("thanhtien = %d\n", thanhtien * 75/100);
+  getchar();
+  return 0;
+}
