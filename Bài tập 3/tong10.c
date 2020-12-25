838915451b32c0f1fa7d03c129d319830a65e85d
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2020-12-25 08:52:10 +0700

    Update tong10.c

--- Bài tập 3/tong10.c
+++ Bài tập 3/tong10.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+ 
+int main()
+{
+    int tong;
+    tong = 0;
+    int dem;
+    for (dem = 1; dem <= 10; dem++)
+    {
+        tong = tong + dem;
+    }
+ 
+    printf("tong: %d", tong);
+ 
+    return 0;
+}
