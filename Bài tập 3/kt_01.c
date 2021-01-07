a94631e026ef61cf1cf063f4fa913f28a16bc904
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2021-01-07 10:46:01 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -1,32 +1,20 @@
-#include <stdio.h>
- 
-int main(){
-    printf("\n---BANG CUU CHUONG---\n");
-    //hien thi ra man hinh BANG CUU CHUONG
-    for(int i = 1; i <= 10;i++){
-        printf("\n---BANG NHAN %d---\n", i);
-        //hien thi ra man hinh BANG NHAN va phep nhan tu 1 den 10
-        for(int j = 0; j < 10; j++){
-            printf("\t%d x %d = %d\n", i, j, i*j);
-        }
-    }
-return 0;
-}
-
-
------------
-#include <stdio.h>
- 
-int main(){
-  int i;
-  int j;
-  printf("nhap bang cuu chuong:");
-  scanf("%d",&i);{
-    if(i=1;i++){
-      printf("bang nhan %d", i);
-    }else if("j = 0;j < 5; j++"){
-      printf("%d * %d = %d\n", i,j,i*j);
-    }
-  }
-  return 0;
+#include<stdio.h>
+int main(void){
+  int a,b;
+  printf("\nnbang cuu chuong\n");
+  //bang nhan 2//
+  printf("\tbang nhan 2\n");
+  printf("2 x 1 = 2\n");
+  printf("2 x 2 = 4\n");
+  printf("2 x 3 = 6\n");
+  //bang cuu chuong x 3//
+  printf("\tbang nhan 3\n");
+  printf("3 x 1 =3\n");
+  printf("3 x 2 =6\n");
+  printf("3 x 3 =9\n");
+  //bang cuu chuong x4//
+  printf("\tbang nhan 4\n");
+  printf("4 x 1 = 4\n");
+  printf("4 x 2 = 8\n");
+  printf("4 x 3 = 12\n");
 }
