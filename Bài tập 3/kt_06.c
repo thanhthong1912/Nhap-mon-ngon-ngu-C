ecc51f74b2ff77b1d37d1c551740194739a876b0
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2021-01-09 10:39:20 +0700

    Update kt_06.c

--- Bài tập 3/kt_06.c
+++ Bài tập 3/kt_06.c
@@ -23,18 +23,18 @@ int main(){
 #iclude <stdio.h>
 int main (){
 /*
-h 4:4 hang
+h = 4 4:4 hang
 hang 1: 1 ngoi sao 6 khoanng trang
 hang 2: 3 ngoi sao 4 khoanng trang
 hang 3: 5 ngoi sao 2 khoanng trang
 hang 4: 7 ngoi sao 0 khoanng trang
 
-h 3:3 hang
+h = 3 3:3 hang
 hang 1: 1 ngoi sao 4 khoanng trang
 hang 2: 3 ngoi sao 2 khoanng trang
 hang 3: 4 ngoi sao 0 khoanng trang
 
-h 2:2 hang 
+h = 2 2:2 hang 
 hang 1: 1 ngoi sao 2 khoanng trang
 hang 2: 3 ngoi sao 0 khoanng trang
 */
