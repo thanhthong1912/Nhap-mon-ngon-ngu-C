c9b1c718393e3a2536452e451e8144efdfb33810
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2021-01-09 10:28:14 +0700

    Update kt_06.c

--- Bài tập 3/kt_06.c
+++ Bài tập 3/kt_06.c
@@ -20,23 +20,25 @@ int main(){
 
 ============
 
-#include<stdio.h>
-int main(){
-  /*
-  h = 4 4:4 hang
-  hang 1: 1 sao 3 khoang trong
-  hang 2: 3 sao 2 khoang trong
-  hang 3: 5 sao 1 khoang trong
-  hang 4: 7 sao 0 khoang trong
-  
-  h = 3 3:3 hang
-  hang 1: 1 sao 2 khoang trong
-  hang 2: 3 sao 1 khoang trong
-  hang 3: 5 sao 0 khoang trong
-  
-  h = 2  2:2 hang
-  hang 1: 1 sao 1 khoang trong
-  hang 2: 3 sao 0 khoang trong
-  */
+#iclude <stdio.h>
+int main (){
+/*
+h 4:4 hang
+hang 1: 1 ngoi sao 6 khoanng trang
+hang 2: 3 ngoi sao 4 khoanng trang
+hang 3: 5 ngoi sao 2 khoanng trang
+hang 4: 7 ngoi sao 0 khoanng trang
+
+h 3:3 hang
+hang 1: 1 ngoi sao 4 khoanng trang
+hang 2: 3 ngoi sao 2 khoanng trang
+hang 3: 4 ngoi sao 0 khoanng trang
+
+h 2:2 hang 
+hang 1: 1 ngoi sao 2 khoanng trang
+hang 2: 3 ngoi sao 0 khoanng trang
+*/
+
+
 return 0;
 }
