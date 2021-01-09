4d07152c268e2f89c903fd03acdcd59c8f5a7181
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2021-01-09 08:38:29 +0700

    Update kt_04.c

--- Bài tập 3/kt_04.c
+++ Bài tập 3/kt_04.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
 	int n,so_du,sum=0;
-	printf("Nhap so n (so nguyen <1000): "); scanf("%d",&n);
+	printf("Nhap so n (so nguyen <1000): "); 
+        scanf("%d",&n);
     
 	for(;n!=0;n=n/10){
 		sum += n%10;
