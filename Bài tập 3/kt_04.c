6670aeb46fc403113a6a022fd63597e2b3c3991e
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2021-01-09 10:16:00 +0700

    Update kt_04.c

--- Bài tập 3/kt_04.c
+++ Bài tập 3/kt_04.c
@@ -1,13 +1,29 @@
 #include<stdio.h>
-int main(){
-	int n,so_du,sum=0;
-	printf("Nhap so n (so nguyen <1000): "); 
-        scanf("%d",&n);
-    
-	for(;n!=0;n=n/10){
-		sum += n%10;
-	}
-	
-	printf("sum = %d\n",sum);
-	return 0;
-}
+ int main() {
+    int n, i, S = 0;
+    printf("nhap n: ");
+    scanf("%d", &n);
+    while (n != 0) {
+        i = n % 10;
+        S += i;
+        n = n / 10;
+    }
+        printf("tong cac chu so cua n la: %d", S);
+    return 0;
+ }
+
+
+#include<stdio.h>
+ int main() {
+ 	int n = 123;
+ 	int s = 0;
+ 	int so ;
+ 	for(int i = 1; n > 0; i++)}
+ 		so = n % 10;
+ 		n = n / 10;
+ 		s = s + so;
+ 		{
+ 		printf("%d",s);
+	 
+ 	return 0;
+ 	}
