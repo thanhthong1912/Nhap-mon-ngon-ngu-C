8cdc109412bdf5a29986035e498c7da4fdb892a1
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2021-01-14 10:21:21 +0700

    Update mang_01.c

--- Bài tập 4/mang_01.c
+++ Bài tập 4/mang_01.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-//gan gia tri va tao mang
+  //gan gia tri va tao mang
   int a[4];
   int i, tong = 0;
-  a[0] = 3;
-  a[1] = 5;
-  a[2] = 7;
-  a[3] = 2;
-	printf("%d ", a[0]);
-  printf("%d ", a[1]);
-  printf("%d ", a[2]);
-  printf("%d ", a[3]);
+  a[0] = 3;a[1] = 5;a[2] = 7;a[3] = 2;
+    printf("%d ", a[0]);
+    printf("%d ", a[1]);
+    printf("%d ", a[2]);
+    printf("%d ", a[3]);
   for(int i = 0; i < 4; i++){
     printf("%d", a[i]);
   }
-//tinh tong
+  //tinh tong
   for(int i = 0; i < 4; i++){
  tong = tong + a[i];
  }
