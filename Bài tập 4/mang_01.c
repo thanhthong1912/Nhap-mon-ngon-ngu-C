6bc5843a9058618af7e0551096bc354f803c0076
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2021-01-14 10:07:06 +0700

    Update mang_01.c

--- Bài tập 4/mang_01.c
+++ Bài tập 4/mang_01.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
-int main()
-{	
-  //khai bao bien va tao mang
-	int array[4] = {3, 5, 7, 2};
-  //khai bao bien i, bien s = 0
-	int i, s = 0;
-  //khai bao bien n = 4
-	int n = 4;
-	{
-  //vong lap xuat ra so 3, 5, 7, 2
-		for ( i = 0; i < n; i++)
-		{
-      //hien thi tren man hinh 3, 5, 7, 2
-			printf("%d", array[i]);
-		}
-	}
-	//tinh tong cac so tu day tren
-	for ( i = 0; i < n; i++)
-	{
-		s = s + array[i];
-	}
-		printf("tong cac so tu day tren = %d\n", s);
+int main(void) {
+//gan gia tri va tao mang
+  int a[4];
+  int i, tong = 0;
+  a[0] = 3;
+  a[1] = 5;
+  a[2] = 7;
+  a[3] = 2;
+	printf("%d ", a[0]);
+  printf("%d ", a[1]);
+  printf("%d ", a[2]);
+  printf("%d ", a[3]);
+  for(int i = 0; i < 4; i++){
+    printf("%d", a[i]);
+  }
+//tinh tong
+  for(int i = 0; i < 4; i++){
+ tong = tong + a[i];
+ }
+//hien thi ra mang hinh tong day so 
+printf(" tong cac so tu day tren = %d\n", tong);
+
 return 0;
 }
