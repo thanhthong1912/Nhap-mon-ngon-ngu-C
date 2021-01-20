3df4d0346c609422ea33ed32c24b4d0c5287f46d
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2021-01-20 18:12:27 +0700

    Update mang_02.c

--- Bài tập 4/mang_02.c
+++ Bài tập 4/mang_02.c
@@ -1,35 +1,3 @@
-#include <stdio.h>
-
-int main () 
-{
-  //Khai bao bien kem khoi tao mang (Chiec hop 100 ngan)
-  int a[100];
-  //Khao bao bien n va i
-  int n, i;
-  //Khai bao bien s de tinh tong
-  int s = 0;
-      //In dong chu nhap kich thuoc mang ra man hinh
-  		printf("Nhap kich thuoc mang: \n");
-      //Nhap so lieu tu ban phim
-  		scanf("%d", &n);
-      //In dong chu Nhap so phan tu ra man hinh
-      printf("Nhap so phan tu: \n");
-      //Vong lap i 
- 	for	(i = 0; i < n; i++)
-		{
-      //Nhap so lieu phan tu ra man hinh
-      scanf("%d", &a [i]);
-		}
-  
-	
-	
-		
-	
-    
-		
-  
-  return  0;
-}
 include <stdio.h>
 
 int main () 
