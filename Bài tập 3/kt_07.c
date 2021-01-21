58cfe76633ef3a3eb865f233e3f8cb0509a293a4
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2021-01-21 07:24:32 +0700

    Update kt_07.c

--- Bài tập 3/kt_07.c
+++ Bài tập 3/kt_07.c
@@ -2,24 +2,21 @@
 int main(){ 
 
 //khai bao bien kieu int int a, b, c 
+int a,b,c;
 
 //hien thi ra man hinh 
 printf("Nhap a =\n");
  //nhap so lieu tu ban phim 
 scanf("%d", &a); 
-//hien thi ra man hinh 
 printf("Nhap b =\n"); 
-//nhap so lieu tu ban phim 
 scanf("%d", &b); 
-//hien thi ra man hinh 
 printf("Nhap c =\n"); 
-//nhap so lieu tu ban phim 
 scanf("%d", &c); 
 if((a - c) < b && b < (a + c)) { 
 printf("La 3 canh cua tam giac"); 
 
 } else { 
-printf("Khong phai la 3 canh cua tam giac"); 
+printf("khong phai la 3 canh cua tam giac"); 
 }
 return 0;
 }
