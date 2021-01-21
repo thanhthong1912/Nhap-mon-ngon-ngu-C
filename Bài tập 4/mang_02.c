637a9dfa5f71af342ae854d1978b1a58346fe178
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2021-01-21 10:29:05 +0700

    Update mang_02.c

--- Bài tập 4/mang_02.c
+++ Bài tập 4/mang_02.c
@@ -1,17 +1,23 @@
 #include<stdio.h>
 
 int main(){
-	int a[100];
-	int i,n, s = 0;
-	printf("nhap kich thuoc: \n");
-	scanf("%d",n);
-	printf("nhap so phan tu: \n");
-	for (i = 0; i < n; i++){
-		scanf("%d",&a [i]);
-		for (i = 0; i < n; i++){
-			s = s + a[i];
-		}
-		printf("tong cac so tu day so tren = %d",s);
-	}
-	return 0;
+    int a[100];//khai bao a (chiec hop 100 ngan)
+    int i,n;// khai bao i va n
+    printf("nhap kich thuoc n = ");// hien thi ra man hinh 
+    scanf("%d", &n);//nhap du lieu 
+    for(i = 0; i < n; i++){
+      printf("\n nhap phan tu a [%d] = ", i);
+      scanf("%d", &a [i]);
+  }
+    printf(" mang a : ");
+    for(i = 0; i < n; i++){ 
+      printf("%d ", a[i]);
+  }
+  n = 3;
+  int tong = 0;
+  for(int j = 0; j < n; j++ ){
+  tong = tong + a[j];
+  }
+  printf("\n tong : %d", tong);
+  return 0;
 }
