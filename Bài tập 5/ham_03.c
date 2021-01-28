6ddc80126f4188cabe03fe0e78866ee22f59c937
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2021-01-28 14:17:13 +0700

    Update ham_03.c

--- Bài tập 5/ham_03.c
+++ Bài tập 5/ham_03.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 
 void Nhap_pt_mang(int a[]);
+int n;
 void Hien_thi_pt_mang(int a[]);
 void Hien_thi_tong_chan(int a[]);
 void Dem_chan_le_0(int a[]);
 
  //khoi ham main
-int n;
 int main(){
-
   printf("Nhap n = ");
   scanf("%d", &n);
   int a[n];
@@ -19,16 +18,22 @@ int main(){
   return 0;
 }
 //dinh nghia ham
-void nhap_pt_mang(){
-  printf("\n Thap phan tu a [%d] = ", n);
-  scanf("%d", a [n]);
+
+//dinh nghia ham void nhap_pt_mang
+void nhap_pt_mang(int a[]){
+  for (int i = 0; i < n; i++){
+    printf("a[%d] = ", i);
+    scanf("%d ", &a[i]);
+  }
 }
+//dinh nghia ham void hien_thi_pt_mang
 void hien_thi_pt_mang(){
-  printf("pt mang la : %d", a[n]);
+  printf("pt mang la : %d", &a[n]);
   for(int i = 0; i > n; i++){
     printf("%d ", a[n]);
   }
 }
+//dinh nghia ham void hien_thi_tong_chan
 void hien_thi_tong_chan(){
   int tongchan = 0;
   for(int j = 0; j < n; j++ ){
@@ -38,22 +43,11 @@ void hien_thi_tong_chan(){
   printf (" \n");
   printf("\n Tong cac so chan : %d", tongchan);
 }
+//dinh nghia ham void dem_chan_le_0
 void dem_chan_le_0(){
-  //dem cac so chan
   int tongle = 0;
   for(int j = 0; j < n; j++ ){
     if(a[j] % 2 == 1)
       tongle = tongle + a[j];
   }
-  printf (" \n");
-  printf("\n Tong cac so le : %d", tongle);
-{
-  //dem cac so le
-  int demle = 0;
-  for (int l = 0; l < n; l++)
-  if (a[l] % 2 == 1 && a[l] != 0)
-    demle = demle + 1;
-    printf (" \n");
-    printf("\n So cac so le : %d", demle);
-}
 }
