879be228f963849731f3a873d2fdf2dd3987356d
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2021-01-28 14:20:16 +0700

    Update mang_03.c

--- Bài tập 4/mang_03.c
+++ Bài tập 4/mang_03.c
@@ -1,21 +1,42 @@
 #include<stdio.h>
 
+void Nhap_pt_mang(int a[]);
+int n;
+void Hien_thi_pt_mang(int a[]);
+void Hien_thi_tong_chan(int a[]);
+int a[100];
+int i;
+void Dem_chan_le_0(int a[]);
+
+ //khoi ham main
 int main(){
-    int a[100];//khai bao a (chiec hop 100 ngan)
-    int i,n;// khai bao i va n
-    printf(" Nhap kich thuoc n = ");// hien thi ra man hinh 
-    scanf("%d", &n);//nhap du lieu 
-    for(i = 0; i < n; i++){
-      printf("\n Nhap phan tu a [%d] = ", i);
-      scanf("%d", &a [i]);
+  printf("Nhap n = ");
+  scanf("%d", &n);
+  int a[n];
+  Nhap_pt_mang(a);
+  Hien_thi_pt_mang(a);
+  Hien_thi_tong_chan(a);
+  Dem_chan_le_0(a);
+  return 0;
+}
+//dinh nghia ham
+
+//dinh nghia ham void nhap_pt_mang
+void nhap_pt_mang(int a[]){
+  for (int i = 0; i < n; i++){
+    printf("a[%d] = ", i);
+    scanf("%d ", &a[i]);
   }
-  //xuat ra mang
-    printf (" \n");
-    printf(" Mang a : ");
-    for(i = 0; i < n; i++){ 
-      printf("%d ", a[i]);
+}
+//dinh nghia ham void hien_thi_pt_mang
+void hien_thi_pt_mang(){
+  printf("pt mang la : %d", &a[n]);
+  for(int i = 0; i > n; i++){
+    printf("%d ", a[n]);
   }
-  //tinh tong cac so chan
+}
+//dinh nghia ham void hien_thi_tong_chan
+void hien_thi_tong_chan(){
   int tongchan = 0;
   for(int j = 0; j < n; j++ ){
     if(a[j] % 2 == 0)
@@ -23,32 +44,12 @@ int main(){
   }
   printf (" \n");
   printf("\n Tong cac so chan : %d", tongchan);
-{
-  //dem cac so chan
-  int demchan = 0;
-  for (int k = 0; k < n; k++)
-  if(a[k] % 2 == 0 && a[k] != 0)
-    demchan = demchan + 1;
-    printf (" \n");
-    printf(" \n So cac so chan: %d", demchan);
 }
-//tinh tong cac so le
-int tongle = 0;
+//dinh nghia ham void dem_chan_le_0
+void dem_chan_le_0(){
+  int tongle = 0;
   for(int j = 0; j < n; j++ ){
     if(a[j] % 2 == 1)
       tongle = tongle + a[j];
   }
-  printf (" \n");
-  printf("\n Tong cac so le : %d", tongle);
-{
-  //dem cac so le
-  int demle = 0;
-  for (int l = 0; l < n; l++)
-  if (a[l] % 2 == 1 && a[l] != 0)
-    demle = demle + 1;
-    printf (" \n");
-    printf("\n So cac so le : %d", demle);
-}
-
-  return 0;
 }
