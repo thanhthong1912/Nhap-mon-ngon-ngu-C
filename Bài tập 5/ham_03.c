f9fd4c8a84689e9be31ded7b774942ad90eed039
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2021-01-28 10:53:14 +0700

    Update ham_03.c

--- Bài tập 5/ham_03.c
+++ Bài tập 5/ham_03.c
@@ -1,5 +1,3 @@
-
-//khoi bao ham
 #include<stdio.h>
 
 void Nhap_pt_mang(int a[]);
@@ -20,6 +18,42 @@ int main(){
   Dem_chan_le_0(a);
   return 0;
 }
-
-//khoi dinh nghia ham
-
+//dinh nghia ham
+void nhap_pt_mang(){
+  printf("\n Thap phan tu a [%d] = ", n);
+  scanf("%d", a [n]);
+}
+void hien_thi_pt_mang(){
+  printf("pt mang la : %d", a[n]);
+  for(int i = 0; i > n; i++){
+    printf("%d ", a[n]);
+  }
+}
+void hien_thi_tong_chan(){
+  int tongchan = 0;
+  for(int j = 0; j < n; j++ ){
+    if(a[j] % 2 == 0)
+      tongchan = tongchan + a[j];
+  }
+  printf (" \n");
+  printf("\n Tong cac so chan : %d", tongchan);
+}
+void dem_chan_le_0(){
+  //dem cac so chan
+  int tongle = 0;
+  for(int j = 0; j < n; j++ ){
+    if(a[j] % 2 == 1)
+      tongle = tongle + a[j];
+  }
+  printf (" \n");
+  printf("\n Tong cac so le : %d", tongle);
+{
+  //dem cac so le
+  int demle = 0;
+  for (int l = 0; l < n; l++)
+  if (a[l] % 2 == 1 && a[l] != 0)
+    demle = demle + 1;
+    printf (" \n");
+    printf("\n So cac so le : %d", demle);
+}
+}
