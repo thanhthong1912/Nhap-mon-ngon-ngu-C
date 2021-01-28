a1a4fd26a7bf8eca219c6e3b6b6e2bf4acd0b7b9
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2021-01-28 15:02:33 +0700

    Update TH_02.c

--- Bài tập 5/TH_02.c
+++ Bài tập 5/TH_02.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
 
+int main(){
+  //khai bao bien n, S
+	int n;
+	int S;
+	S = 0;
+  //hien thi ra man hinh "nhap n ="
+	printf ("Nhap n = ");
+  //nhap du lieu
+	scanf ("%d",&n);
+	for (int i =1; i<=n; i++){
+		S = S + i;
+	}
+	printf ("S = 1 + 2 + 3 ... + %d = %d",n,S);
+	return 0;
+}
