aeb4106c3c9f26930496d59d6e6fbaa4291e9b13
thanhthong1912 <74332681+thanhthong1912@users.noreply.github.com>
2021-01-28 15:06:50 +0700

    Update TH_03.c

--- Bài tập 5/TH_03.c
+++ Bài tập 5/TH_03.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+ 
+int main(){
+  int a, b, c; //khai bao 3 bien a,b,c
+  //nhap a,b,c
+  printf("Nhap so a: "); 
+  scanf("%d", &a);
+  printf ("Nhap so b: "); 
+  scanf ("%d",&b);
+  printf ("Nhap so C: "); 
+  scanf ("%d",&c);
+  if (a >= b && a >= c){
+    //hien thi ra man hinh a la so lon nhat
+    printf("So lon nhat trong 3 so la:\n a = %d", a);
+}else if (b >= a && b >= c){
+    //hien thi ra man hinh b la so lon nhat
+    printf("So lon nhat trong 3 so la:\n b = %d", b);
+}else{
+    //hien thi ra man hinh c la so lon nhat
+    printf("So lon nhat trong 3 so la:\n c = %d", c);
+    }
+    return 0;
+}
